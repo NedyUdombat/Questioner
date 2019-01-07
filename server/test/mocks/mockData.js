3eedd80f8eeafb0ab165ec6aa6c563859abac273
nedyudombat <nedyudombat@gmail.com>
2019-01-07 22:30:13 +0100

    [chore #163008843]Update test and mock meetup test data

--- server/test/mocks/mockData.js
+++ server/test/mocks/mockData.js
@@ -81,31 +81,4 @@ const mockVoteDetails = {
   },
 };
 
-const mockUserDetails = {
-  adminUser: {
-    id: 1,
-    firstname: 'admin',
-    lastname: 'admin',
-    othername: 'admin',
-    email: 'admin@questioner.com',
-    phoneNumber: '08025137999',
-    username: 'admin',
-    password: 'admin',
-    registered: new Date(),
-    isAdmin: true,
-  },
-  normalUser: {
-    id: 2,
-    firstname: 'Nedy',
-    lastname: 'Udombat',
-    othername: 'Edidiong',
-    email: 'nedyudombat@gmail.com',
-    phoneNumber: '08025137999',
-    username: 'nedyy',
-    password: 'qwertyuiop',
-    registered: new Date(),
-    isAdmin: false,
-  },
-};
-
-export { mockMeetupDetails, mockQuestionDetails, mockRSVPDetails, mockVoteDetails, mockUserDetails, mockEmptyUpcomingMeetups };
+export { mockMeetupDetails, mockQuestionDetails, mockRSVPDetails, mockVoteDetails, mockEmptyUpcomingMeetups };
