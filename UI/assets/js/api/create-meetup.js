72ecc2393d96f9ead4134f779b223c090191a95f
nedyudombat <nedyudombat@gmail.com>
2019-02-13 12:01:36 +0100

    [feat #163909314]Consume GET all meetups endpoint for admin

---
+++ UI/assets/js/api/create-meetup.js
@@ -0,0 +1,44 @@
+
+const create = (meetup) => {
+  const token = localStorage.getItem('token');
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(meetup),
+    headers: new Headers({
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'x-access-token': token,
+    }),
+  };
+
+  return fetch('http://127.0.0.1:8080/api/v1/meetups', options)
+    .then(res => res.json())
+    .then((res) => {
+      if (res.status === 201) {
+        setTimeout(() => {
+          document.location.reload();
+          // window.location.assign('meetups.html');
+        }, 2000);
+      }
+      document.querySelector('.error').style.display = 'flex';
+      document.querySelector('.error').innerHTML = res.message;
+    })
+    .catch((err) => {
+
+    });
+};
+
+const createMeetup = (event) => {
+  event.preventDefault();
+  const formData = new FormData(document.querySelector('#createMeetup'));
+  const meetup = {};
+  for (const [key, value] of formData.entries()) {
+    console.log(meetup[key] = value);
+  }
+
+  create(meetup);
+};
+
+document.querySelector('#createMeetup').addEventListener('submit', (event) => {
+  createMeetup(event);
+});
---
+++ UI/assets/js/api/single-meetups.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', () => {
+  let meetupId = document.location.search.split('meetup/')[1];
+  meetupId = Number(meetupId);
+  console.log(meetupId);
+
+  const token = localStorage.getItem('token');
+  const options = {
+    method: 'GET',
+    headers: new Headers({
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'x-access-token': token,
+    }),
+  };
+  fetch(`http://127.0.0.1:8080/api/v1/meetups/${meetupId}`, options)
+    .then(res => res.json())
+    .then((res) => {
+      console.log(res);
+      // let output = '';
+      // res.data.forEach((meetup) => {
+      //   output += `<div class="col span_1_of_3">
+      //       <div class="d-flex justify-content-center">
+      //         <div class="product-card">
+      //           <a href="single-meetup.html?meetup=" + ${meetup.id}">
+      //             <div class="card-image-top">
+      //               <img src="assets/images/meetup1.jfif" alt="empty cart" class="product-image">
+      //             </div>
+      //             <div class="product-card-details">
+      //               <p class="f-20 "> <b>${meetup.topic}</b> </p>
+      //               <div class="d-flex justify-content-between mt-1">
+      //                 <small class="f-12"><i>${meetup.organizer_name}</i></small>
+      //                 <p class="text-blue">${moment(meetup.happening_on).format('DD MMMM YYYY')}</p>
+      //               </div>
+      //             </div>
+      //             <div class="product-card-footer d-flex justify-content-around align-items-center w-100">
+      //               <button type="button" name="button" class="btn bg-transparent w-100"><a href="single-meetup.html"><i class="fas fa-eye fa-lg fa-fw"></i> </a></button>
+      //               <button type="button" name="button" class="btn bg-transparent w-100"><i class="fas fa-edit fa-lg fa-fw text-blue" onclick="launchModal('editModal');"></i> </button>
+      //               <button type="button" name="button" class="btn bg-transparent w-100"><i class="fas fa-trash fa-lg fa-fw text-red" onclick="launchModal('deleteMeetupModal');"></i> </button>
+      //             </div>
+      //           </a>
+      //         </div>
+      //       </div>
+      //     </div>`;
+      //   const meetupContainer = document.querySelector('.section');
+      //   meetupContainer.innerHTML = output;
+      // });
+    });
+});
